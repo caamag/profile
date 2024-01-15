528629af52ea06f5a94c1923819d0c98d71bc58c
BCR-0632 <caiolopesfv@gmail.com>
2024-01-15 16:40:28 -0300

    eniando atualizações

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,8 +6,21 @@ function Home () {
     return <div className="home container">
 
         <div className='home-text'>
-
             <h1>Desenvolvimento <br /><span>Web</span></h1>
+        </div>
+
+        <div className='icons-container'>
+
+            <a href='https://github.com/caamag' target='_blank'>
+                <img src='./assets/github-icon.png' alt='github icon'/>
+            </a>
+
+            <a href='https://www.linkedin.com/in/caio-lopes-programador/' 
+                target='_blank'>
+                <img src='./assets/linkedin-icon.png' alt='linkedin icon'/>
+            </a>
+
+            <a><p>CV</p></a>
 
         </div>
 
--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -1,11 +1,57 @@
 
 import './projetos.css'; 
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function Projetos () {
 
+    useEffect(() => {
+
+        async function getProjectsData (){
+            const res = await fetch('./projetos.json');
+            const data = await res.json(); 
+            const slide1 = data.slide1; 
+            const slide2 = data.slide2;
+            setProjects1(slide1); 
+            setProjects2(slide2); 
+        }
+
+        getProjectsData();
+
+    }, []); 
+
+    const [projects1, setProjects1] = useState([]);
+    const [projects2, setProjects2] = useState([]);
+
     return <div className="projetos container">
 
+        <section className='slide'>
 
+            {projects1.map((project) => (
+                <a href={project.link}>
+                    <div className='item' key={project.id}>
+                        <img alt='project image'
+                        src={project.img}/>
+                        <h1>{project.name}</h1>
+                    </div>
+                </a>
+            ))}
+
+        </section>
+
+        <section className='slide'>
+
+            {projects2.map((project) => (
+                <a href={project.link}>
+                    <div className='item' key={project.id}>
+                        <img alt='project image'
+                        src={project.img}/>
+                        <h1>{project.name}</h1>
+                    </div>
+                </a>
+            ))}
+            
+        </section>
 
     </div>
 
