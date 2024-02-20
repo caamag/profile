8ead2906bf74927240e1cd15180fa19193ff359f
Caio Lopes <caiolopesfv@gmail.com>
2024-02-20 20:02:54 -0300

    removendo erros

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import Home from './pages/home';
 import Sobre from './pages/sobre';
 import Projetos from './pages/projetos';
-import DetailsProject from './pages/detailsProject';
 import Contato from './pages/contato';
 
 //components
 import Navbar from './components/navbar';
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -18,15 +17,14 @@ function App() {
 
       <BrowserRouter>
 
-          <Navbar />
+        <Navbar />
 
         <Routes>
 
-          <Route path='/' element={<Home />}/>
-          <Route path='/sobre' element={<Sobre />}/>
-          <Route path='/projetos' element={<Projetos />}/>
-          <Route path='/projetos/:id' element={<DetailsProject />}/>
-          <Route path='/contato' element={<Contato />}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<Sobre />} />
+          <Route path='/projetos' element={<Projetos />} />
+          <Route path='/contato' element={<Contato />} />
 
         </Routes>
 
--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -1,77 +1,9 @@
 
-import './projetos.css'; 
-import { useEffect } from 'react';
-import { useState } from 'react';
+import './projetos.css';
 
-import { Link } from 'react-router-dom';
-
-function Projetos () {
-
-    //projects information
-    useEffect(() => {
-
-        async function getProjectsData (){
-            const res = await fetch('./projetos.json');
-            const data = await res.json(); 
-            const slide1 = data.slide1; 
-            const slide2 = data.slide2;
-            setProjects1(slide1); 
-            setProjects2(slide2); 
-            
-        }
-
-        getProjectsData();
-
-    }, []); 
-
-    const [projects1, setProjects1] = useState([]);
-    const [projects2, setProjects2] = useState([]);
-
-    //show other projects
-    const [nxtSlide, setNxtSlide] = useState(true);
-    const slideStyle = nxtSlide ? 
-    'slide':
-    'slide slide1'; 
+function Projetos() {
 
     return <div className="projetos container">
-
-        <section className={slideStyle}>
-
-            {projects1.map((project) => (
-                <a href={`/projetos/${project.id}`} className='link-container'>
-                    <div className='item' key={project.id}>
-                        <img alt='project image'
-                        src={project.img}/>
-                        <h1>{project.name}</h1>
-                    </div>
-                </a>
-            ))}
-
-            <button className='prev-btn'
-            onClick={() => {setNxtSlide(false)}}>
-                <img src="./assets/next-btn.png" alt="" />
-            </button>
-
-            <button className='next-btn'
-            onClick={() => {setNxtSlide(true)}}>
-                <img src="./assets/prev-btn.png" alt="" />
-            </button>
-
-        </section>
-
-        <section className='slide'>
-
-            {projects2.map((project) => (
-                <a href={`/projetos/${project.id}`} className='link-container'>
-                    <div className='item' key={project.id}>
-                        <img alt='project image'
-                        src={project.img}/>
-                        <h1>{project.name}</h1>
-                    </div>
-                </a>
-            ))}
-            
-        </section>
 
     </div>
 
